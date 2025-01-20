333f8659df0306b4b7ad4dfbbe92f75a9cc62c76
bean1352 <braundean11@gmail.com>
2025-01-20 19:11:59 +0200

    meta data image fix

--- components/generate-page-metadata.tsx
+++ components/generate-page-metadata.tsx
@@ -1,14 +1,29 @@
 import { Metadata } from "next";
 
+const domain = process.env.NEXT_PUBLIC_DOMAIN;
+const companyName = process.env.NEXT_PUBLIC_COMPANY_NAME;
+
 export function generatePageMetadata(title: string, description: string): Metadata {
     return {
         title,
         description,
         openGraph: {
             description,
+            url: `https://${domain}`,
+            type: "website",
+            images: [
+              {
+                url: `https://${domain}/logo.png`,
+                width: 1200,
+                height: 630,
+                alt: `${companyName}`,
+              },
+            ],
+            siteName: `${companyName}`,
         },
         twitter: {
             description,
+            images: [`https://${domain}/logo.png`]
         }
     };
 }
